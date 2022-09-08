07e7c63ecebb18d5fcd7cf940fcd13810ae3e14a
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-09-08 08:38:22 +0200

    Add GUI Signed-off-by: Federico Bruzzone

---
+++ Source/GUI/PluginEditor.h
@@ -0,0 +1,73 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 6.1.6
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2020 - Raw Material Software Limited.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include <JuceHeader.h>
+#include "../PluginProcessor.h"
+#include "MyTheme.h"
+
+typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
+typedef AudioProcessorValueTreeState::ButtonAttachment ButtonAttachment;
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class PluginEditor  : public juce::AudioProcessorEditor
+{
+public:
+    //==============================================================================
+    PluginEditor (Chorus_effectAudioProcessor& p, AudioProcessorValueTreeState& vts);
+    ~PluginEditor() override;
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.s
+    Chorus_effectAudioProcessor& processor;
+    AudioProcessorValueTreeState& valueTreeState;
+
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
